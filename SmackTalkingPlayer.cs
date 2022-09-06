a22dc7b4a1a5cc9a7036c3a7cad37cf2087df090
Mason Thieman <masonthieman@gmail.com>
2022-09-06 13:46:10 -0500

    Completed SmackTalkingPlayer

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,17 @@
+using System;
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
         public string Taunt { get; }
+
+        public override int Roll()
+        {
+            Console.WriteLine(Taunt);
+            return base.Roll();
+        }
     }
 }
